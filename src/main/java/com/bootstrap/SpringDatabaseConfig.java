ddbff6a60774ca955018926ecd948dc3909cdb4d
Andrew Hrytsiv <andrewhrytsiv@gmail.com>
2016-09-01 01:20:08 +0300

    add database address.propertie file

--- src/main/java/com/bootstrap/SpringDatabaseConfig.java
+++ src/main/java/com/bootstrap/SpringDatabaseConfig.java
@@ -1,9 +1,12 @@
 package com.bootstrap;
 
+import javax.annotation.Resource;
 import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import com.dao.UrlDataDAO;
@@ -12,20 +15,25 @@ import com.dao.sql.UrlDataDAOSql;
 import com.dao.sql.UserDAOSql;
 
 @Configuration
+@PropertySource("classpath:address.properties")
+//"file:${app.home}/app.properties"
 public class SpringDatabaseConfig {
 	
-	private static final String DRIVER_NAME = "org.postgresql.Driver";
-	private static final String URL = "jdbc:postgresql://localhost/test_db";
-	private static final String USERNAME = "andrew";
-	private static final String PASSWORD = "andrew";
+	private static final String DRIVER_NAME = "db.driver";
+	private static final String URL = "db.url";
+	private static final String USERNAME = "db.username";
+	private static final String PASSWORD = "db.password";
+	
+	@Resource
+    private Environment env;
 	
 	@Bean
     public DataSource dataSource() {
 		 DriverManagerDataSource dataSource = new DriverManagerDataSource();
-         dataSource.setDriverClassName(DRIVER_NAME);
-         dataSource.setUrl(URL);
-         dataSource.setUsername(USERNAME);
-         dataSource.setPassword(PASSWORD);
+         dataSource.setDriverClassName(env.getProperty(DRIVER_NAME));
+         dataSource.setUrl(env.getProperty(URL));
+         dataSource.setUsername(env.getProperty(USERNAME));
+         dataSource.setPassword(env.getProperty(PASSWORD));
          return dataSource;
     }
 	
