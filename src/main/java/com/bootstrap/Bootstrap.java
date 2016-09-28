cd85a4bd4bf81c38ea55c8fb412a2200fcc751d0
Andrew Hrytsiv <andrewhrytsiv@gmail.com>
2016-09-28 03:16:30 +0300

    set bootstrap for heroku

--- src/main/java/com/bootstrap/Bootstrap.java
+++ src/main/java/com/bootstrap/Bootstrap.java
@@ -3,6 +3,10 @@ package com.bootstrap;
  
 import static spark.Spark.*;
 
+import java.io.File;
+import java.net.URISyntaxException;
+import java.security.CodeSource;
+
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import com.resource.AuthenticationResource;
@@ -12,19 +16,30 @@ import com.service.UrlDataService;
 
 public class Bootstrap {
 	
-	private static final int PORT = 8090;
+	private static final int DEFAULT_PORT = 4567;
 	public static final String API_CONTEXT = "/api";
  
-    public static void main(String[] args) {
-//    	 Spark.setSecure("deploy/private/localhost.jks", "andrew", null, null); //https set up
-         port(PORT);
-         //run from eclipse
-         //staticFileLocation("./public");
-         //run from jar
-         externalStaticFileLocation(System.getProperty("app.home")+"/public");
+    public static void main(String[] args) throws URISyntaxException {
+         port(getHerokuAssignedPort());
+         externalStaticFileLocation(getPublicFolderPath());
          AnnotationConfigApplicationContext context = initSpringConfigs();
          new AuthenticationResource(context.getBean(AuthenticationService.class));
          new UrlDataResource(context.getBean(UrlDataService.class));
+    }
+    
+    private static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return DEFAULT_PORT;
+    }
+    
+    private static String getPublicFolderPath() throws URISyntaxException{
+    	CodeSource codeSource = Bootstrap.class.getProtectionDomain().getCodeSource();
+    	File jarFile = new File(codeSource.getLocation().toURI().getPath());
+    	String jarDir = jarFile.getParentFile().getPath();
+    	return jarDir + File.separator + "public";
     }
 
 	private static AnnotationConfigApplicationContext initSpringConfigs() {
