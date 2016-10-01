9300ae40e3e201b21fa78e028b5afa4d4a5d69e4
Andrew Hrytsiv <andrewhrytsiv@gmail.com>
2016-10-01 17:07:12 +0300

    start dashboard development

---
+++ src/main/java/com/resource/DashboardResource.java
@@ -0,0 +1,8 @@
+package com.resource;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DashboardResource {
+	private final static Logger LOGGER = LoggerFactory.getLogger(DashboardResource.class);
+}
