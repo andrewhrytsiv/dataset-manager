4197a8a1f725d0c5974b64978590fa36acd18336
andrew hrytsiv <andrewhrytsiv@gmail.com>
2016-10-13 12:19:11 +0300

    Add route titles and implement routes without #

--- src/main/resources/public/app.js
+++ src/main/resources/public/app.js
@@ -8,23 +8,37 @@ var app = angular.module('client_app', [
     'ngFileUpload'
 ]);
 
-app.config(function ($routeProvider) {
+app.config(function ($routeProvider,$locationProvider) {
     $routeProvider.when('/', {
+        title: 'Home',
         templateUrl: 'home/home.html',
         controller: 'HomeController'
     }).when('/login',{
+        title: 'Login',
         templateUrl: 'login/login.html',
         controller: 'LoginController'
     }).when('/register',{
+        title: 'Register',
         templateUrl: 'register/register.html',
         controller: 'RegisterController'
     }).when('/dashboard', {
+        title: 'Dashboard',
         templateUrl: 'dashboard/dashboard.html',
         controller: 'DashboardController'
     }).otherwise({
+        title: 'Home',
         redirectTo: '/'
-    })
+    });
+    // use the HTML5 History API
+    $locationProvider.html5Mode(true);
 });
+
+app.run(['$rootScope', function($rootScope) {
+    $rootScope.$on('$routeChangeSuccess', function (event, current) {
+        $rootScope.title = current.$$route.title;
+    });
+}]);
+
 /*
 app.run(function($rootScope, $http, $location, $localStorage){
     // keep user logged in after page refresh
