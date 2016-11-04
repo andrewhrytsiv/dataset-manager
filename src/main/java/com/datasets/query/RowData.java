74e5c105349aa2e3c795a323f789d1bec4bc8fda
andrew hrytsiv <andrewhrytsiv@gmail.com>
2016-11-04 19:21:35 +0200

    changes from work PC

--- src/main/java/com/datasets/query/RowData.java
+++ src/main/java/com/datasets/query/RowData.java
@@ -2,6 +2,7 @@ package com.datasets.query;
 
 import java.util.Map;
 
+import com.google.common.collect.ComparisonChain;
 import com.google.common.collect.Maps;
 import com.google.gson.Gson;
 
@@ -14,16 +15,24 @@ public class RowData implements Comparable<RowData>{
 		return this;
 	}
 	
+	public Map<String, String> getProperties(){
+		return keyValue;
+	}
+	
+	@Override
+	public int compareTo(RowData another) {
+		ComparisonChain chain = ComparisonChain.start();
+		keyValue.entrySet().forEach( entry -> {
+			String key = entry.getKey();
+			chain.compare(entry.getValue(), another.getProperties().get(key));
+		});
+		return  chain.result();
+	}
+	
 	@Override
 	public String toString(){
 		Gson gson = new Gson(); 
 		return gson.toJson(keyValue);
-	}
-	
-	@Override
-	public int compareTo(RowData o) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+	}	
 
 }
