e6dd18cfd52127fdd35c70b4b0d636a3f9415575
andrew hrytsiv <andrewhrytsiv@gmail.com>
2016-11-10 15:05:01 +0200

    refactor

--- src/main/java/com/dao/sql/DatasetDAOSql.java
+++ src/main/java/com/dao/sql/DatasetDAOSql.java
@@ -1,8 +1,8 @@
 package com.dao.sql;
 
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.sql.DataSource;
 
@@ -47,21 +47,14 @@ public class DatasetDAOSql implements DatasetDAO{
 	
 	private void insert(Dataset dataset) throws DataAccessException, SQLException{
 		String sql = "INSERT INTO datasets (dataset_id, json_data, personal, snapshot_date, owner) VALUES (?, ?, ?, ?, ?)";
-		jdbcTemplate.update(sql, new Object[] { dataset.getUuid(), dataset.getPGJson(), dataset.isPersonal(),
-				dataset.getSnapshotDate(), dataset.getOwner() });
+		jdbcTemplate.update(sql, new Object[] {dataset.getUuid(), dataset.getPGJson(), dataset.isPersonal(), dataset.getSnapshotDate(), dataset.getOwner()});
 	}
 	
 	private void insert(MetadataKeyValue metadata){
 		String sql = "INSERT INTO metadata_key_value (key, value, dset_id, table_name) VALUES (?, ?, ?, ?)";
-		List<Object[]> parameters = new ArrayList<Object[]>();
-		metadata.getKeyValue().entrySet().forEach(entry -> {
-			Object[] row = new Object[4];
-			row[0] = entry.getKey();
-			row[1] = entry.getValue();
-			row[2] = metadata.getUuid();
-			row[3] = metadata.getTable();
-			parameters.add(row);
-		});
+		List<Object[]> parameters = metadata.getKeyValue().entrySet().stream()
+				.map(e -> new Object[] {e.getKey(), e.getValue(), metadata.getUuid(), metadata.getTable()})
+				.collect(Collectors.toList());
 		jdbcTemplate.batchUpdate(sql, parameters);
 	}
 
