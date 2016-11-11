9135790668745bd877c5788826232b0249aa41cd
Andrew Hrytsiv <andrewhrytsiv@gmail.com>
2016-11-11 23:29:31 +0200

    add filter in bootstrap

--- src/main/java/com/bootstrap/Bootstrap.java
+++ src/main/java/com/bootstrap/Bootstrap.java
@@ -1,8 +1,21 @@
 package com.bootstrap;
 
 import static spark.Spark.*;
+import static com.util.HTTPHelper.*;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.filter.ProtectedFilter;
+import com.google.common.base.Strings;
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import com.service.AuthenticationService;
+
+
 import static spark.debug.DebugScreen.enableDebugScreen;
+
+import java.util.Date;
+import java.util.Map;
 
 public class Bootstrap {
 	
@@ -13,9 +26,14 @@ public class Bootstrap {
          port(PORT);
          //specify path to public directory
          externalStaticFileLocation(System.getProperty("app.home")+"/public");
+         setFilters();
          initSpringConfigs();
          enableDebugScreen();
     }
+
+	private static void setFilters() {
+		before(API_CONTEXT + "/protected/*", new ProtectedFilter());
+	}
 
 	private static AnnotationConfigApplicationContext initSpringConfigs() {
 		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
