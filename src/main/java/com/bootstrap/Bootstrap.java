950dd836910d4509628fca0d75fc02048a10d51b
andrew hrytsiv <andrewhrytsiv@gmail.com>
2016-12-02 15:10:30 +0200

    [datasets-scheduled-update] bootstrap

--- src/main/java/com/bootstrap/Bootstrap.java
+++ src/main/java/com/bootstrap/Bootstrap.java
@@ -1,14 +1,15 @@
 package com.bootstrap;
 
 import static spark.Spark.*;
-import static spark.debug.DebugScreen.enableDebugScreen;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import com.filter.ProtectedFilter;
+import com.scheduler.DatasetsScheduledService;
 
 public class Bootstrap {
 	
 	private static final int PORT = 8090;
 	public static final String API_CONTEXT = "/api";
+	public static AnnotationConfigApplicationContext springContext;
  
     public static void main(String[] args) {
          port(PORT);
@@ -16,18 +17,22 @@ public class Bootstrap {
          externalStaticFileLocation(System.getProperty("app.home")+"/public");
          setFilters();
          initSpringConfigs();
-         enableDebugScreen();
+         startAsyncServices();
     }
+
+	private static void startAsyncServices() {
+		springContext.getBean(DatasetsScheduledService.class).startAsync();
+	}
 
 	private static void setFilters() {
 		before(API_CONTEXT + "/protected/*", new ProtectedFilter());
 	}
 
 	private static AnnotationConfigApplicationContext initSpringConfigs() {
-		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
-        context.register(SpringAppConfig.class);
-        context.register(SpringDatabaseConfig.class);
-        context.refresh();
-		return context;
+		springContext = new AnnotationConfigApplicationContext();
+		springContext.register(SpringAppConfig.class);
+		springContext.register(SpringDatabaseConfig.class);
+		springContext.refresh();
+		return springContext;
 	}
 }
